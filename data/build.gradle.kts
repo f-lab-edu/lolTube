f2af727d090c0ea213e0f2d83914dceaab0636f5
juseong <wjdwntjd55@naver.com>
2025-07-06 20:32:40 +0900

    feat : data 모듈 추가

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,20 +1,17 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
 }
 
 android {
-    namespace = "com.flab.loltube"
-    compileSdk = 35
+    namespace = "com.flab.data"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.flab.loltube"
         minSdk = 21
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -36,13 +33,10 @@ android {
 }
 
 dependencies {
-    implementation(project(":data"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
