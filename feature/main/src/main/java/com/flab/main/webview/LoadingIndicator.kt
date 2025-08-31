4078f5db22b7dd491b824f8d774b49b1b5e9707b
juseong <wjdwntjd55@naver.com>
2025-08-31 23:02:19 +0900

    review : LoadingIndicator preview 추가

--- feature/main/src/main/java/com/flab/main/webview/LoadingIndicator.kt
+++ feature/main/src/main/java/com/flab/main/webview/LoadingIndicator.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.zIndex
 
 @Composable
@@ -35,4 +36,10 @@ fun LoadingIndicator(
             color = MaterialTheme.colorScheme.primary
         )
     }
+}
+
+@Preview
+@Composable
+private fun LoadingIndicatorPreview() {
+    LoadingIndicator()
 }
